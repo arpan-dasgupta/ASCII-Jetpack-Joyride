0d2bf35d3c06b790a600c6a0bc96f59c866badd5
arpan-dasgupta <arpandasgupta0@gmail.com>
2020-01-19 00:07:30 +0530

    pv 4

--- diag_obstacles.py
+++ diag_obstacles.py
@@ -7,36 +7,33 @@ import config
 
 class DiagObstacle(Moving):
 
-    ele = []
-    size = 0
-
     def __init__(self, size, inipos, direction):
-        self.size = size
+        self.__size = size
         self._posval = inipos
-        self.ele = []
+        self.__ele = []
         for i in range(size):
             if direction == 1:
                 obj = Obstacle(1, [inipos[0]+i, inipos[1]+i])
             else:
                 obj = Obstacle(1, [inipos[0]+size-1-i, inipos[1]+i])
-            self.ele.append(obj)
+            self.__ele.append(obj)
         self.array = np.full((size, 1, 1), '#')
         self.msk = np.full(np.shape(self.array), Fore.RED)
 
     def update_pos(self):
-        # print(self._posval, self._posval[1] -
-        #       self.ele[0].get_pos()[1], len(self.ele))
+        # print(self.posval, self.posval[1] -
+        #       self.__ele[0].get_pos()[1], len(self.__ele))
         self._posval[1] -= 2 + 2 * (config.SPEED_UP == 1)
-        for element in self.ele:
+        for element in self.__ele:
             element.update_pos()
 
     def body(self):
         lli = []
-        for array in self.ele:
+        for array in self.__ele:
             good = array.body()
             lli.append(good[0])
         return lli
 
     def del_ob(self):
-        for elem in self.ele:
+        for elem in self.__ele:
             del elem
--- hor_obstacles.py
+++ hor_obstacles.py
@@ -15,7 +15,7 @@ class HorObstacle(Obstacle):
 
 def obstacle_gen():
     baseh = np.random.randint(0, SCREENHEIGHT - 7)
-    key_val = np.random.randint(0, 3)
+    key_val = np.random.randint(0, 1)
     if key_val == 1:
         obs = Obstacle(np.random.randint(4, 7), [baseh, SCREENWIDTH-1])
     elif key_val == 0:
